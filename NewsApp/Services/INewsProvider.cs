703aa4720e8a12fb0e36ecade79be70a8a50866d
Linerath <linerath@mail.ru>
2017-12-06 20:59:44 +0300

    Added Services/NewsStorage

---
+++ NewsApp/Services/INewsProvider.cs
@@ -0,0 +1,17 @@
+﻿using NewsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsApp.Services
+{
+    public interface INewsProvider
+    {
+        IEnumerable<News> GetNews();
+        News GetNews(int id);
+        void AddNews(News news);
+        void DeleteNews(int id);
+        void UpdateNews(News news);
+    }
+}
---
+++ NewsApp/Services/NewsStorage.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NewsApp.Models;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+using Dapper;
+
+namespace NewsApp.Services
+{
+    public class NewsStorage : INewsProvider
+    {
+        private string connectionString;
+
+        public NewsStorage(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("MySqlConnectionString");
+        }
+
+        public void AddNews(News news)
+        {
+            using (IDbConnection db = new MySqlConnection(connectionString))
+            {
+                var sqlQuery = "INSERT INTO news (heading, text, creationDate, category) VALUES(@Heading, @Text, @CreationDate, @Category)";
+                db.Execute(sqlQuery, news);
+            }
+        }
+
+        public void DeleteNews(int id)
+        {
+            using (IDbConnection db = new MySqlConnection(connectionString))
+            {
+                var sqlQuery = "DELETE FROM news WHERE id = @id";
+                db.Execute(sqlQuery, new { id });
+            }
+        }
+
+        public IEnumerable<News> GetNews()
+        {
+            using (IDbConnection db = new MySqlConnection(connectionString))
+            {
+                return db.Query<News>("SELECT * FROM news").ToList();
+            }
+        }
+
+        public News GetNews(int id)
+        {
+            using (IDbConnection db = new MySqlConnection(connectionString))
+            {
+                return db.Query<News>("SELECT * FROM news WHERE id = @id", new { id }).FirstOrDefault();
+            }
+        }
+
+        public void UpdateNews(News news)
+        {
+            using (IDbConnection db = new MySqlConnection(connectionString))
+            {
+                var sqlQuery = "UPDATE news SET heading = @Heading, text = @Text, creationDate = @CreationDate, category = @Category WHERE id = @Id";
+                db.Execute(sqlQuery, news);
+            }
+        }
+    }
+}
